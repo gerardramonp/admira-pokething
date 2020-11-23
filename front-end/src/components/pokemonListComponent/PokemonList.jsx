3cef5068c5095da958fee37ed4e7b2be852470d1
gerard <gerardramonp@gmail.com>
2020-11-23 21:04:32 +0100

    add basic compos & redux config. Linter not formatting

---
+++ front-end/src/components/pokemonListComponent/PokemonList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import "PokemonList.scss";
+
+function PokemonList({ pokemonList, dispatch }) {
+    return <p>List works...</p>;
+}
+
+function mapStateToProps({ pokeReducer }) {
+    return {
+        pokemonList: pokeReducer.pokemonList,
+    };
+}
+
+export default connect(mapStateToProps)(PokemonList);
