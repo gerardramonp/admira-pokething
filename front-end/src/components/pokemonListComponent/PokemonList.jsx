c8f5fc542f1539a145f4980b489555cf009cf9d9
gerard <gerardramonp@gmail.com>
2020-11-23 21:34:54 +0100

    add components and reduces

--- front-end/src/components/pokemonListComponent/PokemonList.jsx
+++ front-end/src/components/pokemonListComponent/PokemonList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import 'PokemonList.scss';
+import './PokemonList.scss';
 
 function PokemonList({ pokemonList, dispatch }) {
   return <p>List works...</p>;
