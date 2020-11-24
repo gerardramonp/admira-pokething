3cb5985e7b5e66abb931f93a58033c6b4b2ae739
gerard <gerardramonp@gmail.com>
2020-11-24 20:46:54 +0100

    add responsive design to header

--- front-end/src/components/HeaderComponent/Header.jsx
+++ front-end/src/components/HeaderComponent/Header.jsx
@@ -6,14 +6,16 @@ function Header() {
     <>
       <div className="header">
         <div className="header__item header__item--left">
-          <img src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png" alt="pokedex-circle" className="header__logo" />
-          <div className="pokedex__buttons">
-            <div className="button button--red" />
-            <div className="button button--yellow" />
-            <div className="button button--green" />
+          <div className="header__decorators">
+            <img src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png" alt="pokedex-circle" className="header__logo" />
+            <div className="pokedex__buttons">
+              <div className="button button--red" />
+              <div className="button button--yellow" />
+              <div className="button button--green" />
+            </div>
           </div>
         </div>
-        <div className="header__item">
+        <div className="header__item desktop">
           <img src="https://ianars.github.io/Pok-dex/images/pokedeex.png" alt="pokedex-logo" className="header__logo" />
         </div>
         <div className="header__item">
