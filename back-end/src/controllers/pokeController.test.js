d17819190adf0ac7ea000d0d280758765f05e332
gerard <gerardramonp@gmail.com>
2020-11-24 22:54:34 +0100

    Add getPokemonById controller tests

--- back-end/src/controllers/pokeController.test.js
+++ back-end/src/controllers/pokeController.test.js
@@ -1,6 +1,8 @@
-const pokeModel = require('../models/pokeModel');
+const axios = require('axios');
 const PokeModel = require('../models/pokeModel');
 const pokeController = require('./pokeController')(PokeModel);
+
+jest.mock('axios');
 
 describe('PokeController tests', () => {
   describe('getPokemons method tests', () => {
@@ -34,6 +36,78 @@ describe('PokeController tests', () => {
       pokeController.getPokemons(req, res);
 
       expect(res.json).toHaveBeenCalledWith(pokemonList);
+    });
+  });
+
+  describe('getPokemonById method tests', () => {
+    afterEach(() => {
+      axios.get.mockClear();
+    });
+
+    test('Should call res.send with an error when axios throws error', async () => {
+      const req = { params: 1 };
+      const res = {
+        send: jest.fn(),
+      };
+
+      axios.get.mockReturnValueOnce(Promise.reject(new Error('Error loading pokemon')));
+
+      await pokeController.getPokemonById(req, res);
+
+      expect(res.send.mock.calls[0][0].message).toBe('Error loading pokemon');
+    });
+
+    test('Should call res.send with an error when response has missing properties', async () => {
+      const req = { params: 0 };
+      const res = {
+        send: jest.fn(),
+      };
+
+      axios.get.mockReturnValueOnce(Promise.reject(new Error('Request failed with status code 404')));
+
+      await pokeController.getPokemonById(req, res);
+
+      expect(res.send.mock.calls[0][0].message).toBe('Request failed with status code 404');
+    });
+
+    test('Should call res.json with pokemon data if getting the correct response', async () => {
+      const req = { params: 1 };
+      const res = {
+        json: jest.fn(),
+        send: jest.fn(),
+      };
+
+      const pokeMock = {
+        data: {
+          base_experience: 1,
+          forms: [],
+          game_indices: [],
+          held_items: [],
+          is_default: 1,
+          location_area_encounters: [],
+          order: 1,
+          species: [],
+          sprites: {
+            back_default: 'asd',
+            front_default: 'asd',
+          },
+          randomProps: 'value',
+        },
+      };
+
+      axios.get.mockReturnValueOnce(Promise.resolve(pokeMock));
+
+      await pokeController.getPokemonById(req, res);
+
+      const expectValue = {
+        sprites: {
+          back_default: 'asd',
+          front_default: 'asd',
+        },
+        randomProps: 'value',
+      };
+
+      expect(res.json).toHaveBeenCalledWith(expectValue);
     });
   });
 });
