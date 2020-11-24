3f3b91858518dbd4026474387711ff1a7ac6afcb
gerard <gerardramonp@gmail.com>
2020-11-24 20:38:45 +0100

    add header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PokemonList from './components/pokemonListComponent/PokemonList';
+import Header from './components/HeaderComponent/Header';
 
 function App() {
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
-    <Switch>
-      <Route path="/" component={PokemonList} />
-    </Switch>
+  // eslint-disable-next-line react/jsx-filename-extension
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" component={PokemonList} />
+      </Switch>
+    </>
   );
 }
 
---
+++ front-end/src/components/HeaderComponent/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="header__item header__item--left">
+          <img src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png" alt="pokedex-circle" className="header__logo" />
+          <div className="pokedex__buttons">
+            <div className="button button--red" />
+            <div className="button button--yellow" />
+            <div className="button button--green" />
+          </div>
+        </div>
+        <div className="header__item">
+          <img src="https://ianars.github.io/Pok-dex/images/pokedeex.png" alt="pokedex-logo" className="header__logo" />
+        </div>
+        <div className="header__item">
+          <img src="https://lh3.googleusercontent.com/proxy/NJ5skYgeTVmArBLNZzoMOlV758dH9RWRf8ysvNuC44acr6hsAW4oHj8hEHl4JWNfSB-BHa7wdpTso40q-mWyNrh4lR2z4i-W72oA3MeBTB_63nL8LLl4z7EQffevWI7voLp-pR75WWA" alt="admira-logo" className="header__logo" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
