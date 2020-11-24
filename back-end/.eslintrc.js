53a74933656bb94a7b018c9ba7c9319b79610d3d
gerard <gerardramonp@gmail.com>
2020-11-24 20:07:06 +0100

    back controller getPokemons tested

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
-        node: true,
-    },
-    extends: ["airbnb-base"],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {
-        "no-debugger": 1,
-        "linebreak-style": 0,
-        "global-require": 0,
-        "eslint linebreak-style": [0, "error", "windows"],
-    },
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-debugger': 1,
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+  },
 };
