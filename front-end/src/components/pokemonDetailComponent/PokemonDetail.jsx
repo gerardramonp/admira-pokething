4b73be839695df917194c1ce88c2e7cc221bc0ef
gerard <gerardramonp@gmail.com>
2020-11-25 22:22:38 +0100

    Add back button and styles to detail

--- front-end/src/components/pokemonDetailComponent/PokemonDetail.jsx
+++ front-end/src/components/pokemonDetailComponent/PokemonDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { loadPokemonById } from '../../redux/actions/pokeActions';
 
 import './PokemonDetail.css';
@@ -17,59 +17,59 @@ function PokemonDetail({ pokemonDetail, dispatch }) {
     <>
       { pokemonDetail && pokemonDetail?.id && (
         <div className={`pokemon-detail bg-${pokemonDetail.types[0].type.name}`}>
-          {console.log(pokemonDetail)}
-          <h1 className="pokemon-detail__title">
-            #
-            {' '}
-            {pokemonDetail.id}
-            {' '}
-            -
-            {' '}
-            {pokemonDetail.name}
-          </h1>
-          <main className="pokemon-detail__main">
-            <section className="detail-main__info">
-              <div className="detail-info__types">
-                {pokemonDetail.types.map(({ type }) => <span key={type.name} className={`types__type ${type.name}`}>{type.name}</span>)}
-              </div>
-              <div className="detail-info__sprites">
-                <img src={pokemonDetail.sprites.default__front} alt="front-sprite" className="sprites__item" />
-                <img src={pokemonDetail.sprites.default__back} alt="back-sprite" className="sprites__item" />
-              </div>
-              <h4>Other info:</h4>
-              <div className="detail-info__other">
-                <p>
-                  Height:
-                  {' '}
-                  {pokemonDetail.height}
-                </p>
-                <p className="other__item">
-                  Weight:
-                  {' '}
-                  {pokemonDetail.weight}
-                </p>
-              </div>
-            </section>
-            <section className="main__stats">
-              <h3>Base Stats:</h3>
-              <div className="stats__bars">
-                {/* Aqui fer un map per cada stat posar la seva barra */}
-                {
+          <div className="pokemon-detail__container">
+
+            {console.log(pokemonDetail)}
+            <div className="pokemon-detail__title">
+              <Link to="/">
+                <img src="https://trello-attachments.s3.amazonaws.com/5f7f173f3f927d440950a925/5fbe91ca731763484cbf700b/42db221f6e67c04903a423a3e6e41975/back-button.png" alt="back-to-list" className="back" />
+              </Link>
+
+              <h1 className="title__text title__text--main">
+                {' '}
+                {pokemonDetail.name}
+              </h1>
+              <h1 className="title__text">
+                #
+                {' '}
+                {pokemonDetail.id}
+              </h1>
+            </div>
+            <div className="poke-line" />
+            <main className="pokemon-detail__main">
+              <section className="detail-main__info">
+                <div className="detail-info__types">
+                  {pokemonDetail.types.map(({ type }) => <span key={type.name} className={`types__type ${type.name}`}>{type.name}</span>)}
+                </div>
+                <div className="detail-info__sprites">
+                  <img src={pokemonDetail.sprites.front_default} alt="front-sprite" className="sprites__item" />
+                  <img src={pokemonDetail.sprites.back_default} alt="back-sprite" className="sprites__item" />
+                </div>
+              </section>
+              <section className="main__stats">
+                <h3 className="pokemon-detail__subtitle">Base Stats:</h3>
+                <div className="stats__bars">
+                  {/* Aqui fer un map per cada stat posar la seva barra */}
+                  {
                   pokemonDetail.stats.map((stat) => (
                     <div className="bars__item">
-                      <span>
-                        {stat.stat.name}
+                      <span className="stat-label">
+                        <b>{stat.stat.name}</b>
                         :
+                        {' '}
+                        {stat.base_stat}
                       </span>
-                      <progress value={stat.baseStat} max="100" />
+                      <progress value={stat.base_stat} max="250">stat.base_stat</progress>
                     </div>
                   ))
                 }
 
-              </div>
-            </section>
-          </main>
+                </div>
+              </section>
+            </main>
+          </div>
         </div>
+
       )}
     </>
 
