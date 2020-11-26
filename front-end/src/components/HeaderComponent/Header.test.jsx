78f70a005cd24b52708e68ed7788078e7f6f6b85
gerard <gerardramonp@gmail.com>
2020-11-26 23:17:53 +0100

    remove useles import

--- front-end/src/components/HeaderComponent/Header.test.jsx
+++ front-end/src/components/HeaderComponent/Header.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
 
